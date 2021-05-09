32407924ba98bb51df40fe1942adbbbdea2e2c29
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2021-05-09 18:17:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Works to be done
+
+// neon effect
+// create a toggle swtich for background mode []
+// add picture and self writign and deleting text to introduce myself []
+// timeline feature
+// project cards
+// contact info sideline bars in static mode
