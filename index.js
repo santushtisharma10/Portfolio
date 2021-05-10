0787c54daee54bd056d03ce76dbc8f3aed6eaa0f
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-05-10 17:37:30 +0530

    image changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 // neon effect
 // create a toggle swtich for background mode []
-// add picture and self writign and deleting text to introduce myself []
-// timeline feature
-// project cards
-// contact info sideline bars in static mode
+// typing and deleting text for short intro
+// navbar
+
