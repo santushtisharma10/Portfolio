311aac4d6656225142b9d8559d2fd6ae94dd5dcb
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-05-20 15:13:22 +0530

    changes made

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ console.log("HEllo")
 init()
 function init() {
 
-    var txt = document.querySelector('#text-effect');
+    var txt = document.querySelector('.text-effect');
     const wordList = JSON.parse(txt.getAttribute('data-words'));
     const wait = txt.getAttribute('data-wait');
     console.log(wait);
@@ -63,9 +63,8 @@ function init() {
 }
 
 // Works to be done
-
-// neon effect
+// create a navbar and about me section
 // create a toggle swtich for background mode []
-// typing and deleting text for short intro
-// navbar
+
+
 
