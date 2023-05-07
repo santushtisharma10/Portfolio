e1251deeb3f57ed0ada063639f0f33686b6f9c03
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-07 20:24:41 +0530

    [feature]: various sections added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import Work from "./components/Work"
 
 function App() {
   return (
     <div className="App">
-     Portfolio
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Work />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
   );
 }
