b6dc53fa61da4554d28bfd23f0bdb3cc32c92106
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-08 12:16:17 +0530

    [modification]: navbar styles for desktop view modified

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { styled, Button } from "@mui/material";
+import code from "./giphy.gif";
+import code1 from "./giphy (1).gif";
+
+const NavContainer = styled("div")({
+  padding: "24px 20px 12px",
+  borderBottom: "0.5px solid #F6F1F1",
+  display: "flex",
+  justifyContent: "space-between",
+  fontWeight: '700'
+});
+const Logo = styled("div")({
+  height: "50px",
+});
+const NavMenu = styled("div")({
+  display: "flex",
+  alignItems: "center",
+});
+const NavLink = styled("div")({
+  padding: "0 32px",
+  cursor: "pointer",
+  ":hover": {},
+
+});
+const DownloadButton = styled(Button)({
+  marginLeft: "16px",
+  fontFamily: 'inherit'
+});
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <NavContainer>
+      <Logo>
+        <img
+          style={{ height: "100%" }}
+          src={code}
+          alt="code navbar gif"
+        />
+        {/* <img style={{ height: "100%" }} src={code1} alt="code navbar gif" /> */}
+      </Logo>
+      <NavMenu>
+        <NavLink>About</NavLink>
+        <NavLink>Projects</NavLink>
+        <NavLink>Work</NavLink>
+        <NavLink> Experience</NavLink>
+        <NavLink>Contact</NavLink>
+        <DownloadButton variant="contained">Download Resume</DownloadButton>
+      </NavMenu>
+    </NavContainer>
+  );
 }
 
-export default Navbar
+export default Navbar;
