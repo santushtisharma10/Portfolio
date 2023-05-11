c0d12006901d5715333c5c6cb793240554dd3e31
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-11 15:12:40 +0530

    [modification]: work section header added

--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -1,8 +1,31 @@
+import { Box, Typography, styled } from '@mui/material'
 import React from 'react'
-
+import workIMG from "../images/work.gif"
+const WorkSection = styled(Box)({
+  padding: "50px 100px",
+  backgroundColor: '#285cd1'
+})
+const Header = styled(Box)({
+  display: 'flex', 
+  justifyContent: 'space-between',
+  alignItems: 'center'
+})
+const Title = styled(Typography)({
+  fontSize: "56px",
+  fontWeight: 800,
+  
+})
 function Work() {
   return (
-    <div>Work</div>
+    <WorkSection>
+      <Header>
+        <Title>
+          Work <br/ > <span style={{color: "#80cbe0"}}>Experience</span>
+        </Title>
+        <img style={{height: '200px'}} src={workIMG} />
+      </Header>
+      add 2 cards looking in 1 row, they should like somewhat like horizontal product card
+    </WorkSection>
   )
 }
 
