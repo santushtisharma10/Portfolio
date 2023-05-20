5d60e80e86b877af3d61a6151e3df472bf147e97
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-20 17:17:38 +0530

    [modification]: toggle sidebar added in navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -12,7 +12,13 @@ import {
 import code from "../images/giphy.gif";
 import { Menu, Close } from "@mui/icons-material";
 
-const NavContainer = styled(Box)((theme) => ({
+const MobileNav = styled(Box)({
+  '& .MuiBox-root.active' :{
+    left: 0,
+    backgroundColor: 'pink'
+  }
+})
+const NavContainer = styled(Box)({
   padding: "24px 50px",
   width: "100%",
   display: "flex",
@@ -22,7 +28,7 @@ const NavContainer = styled(Box)((theme) => ({
   fontWeight: 600,
   zIndex: 1000,
   backgroundColor: "white",
-}));
+});
 const Logo = styled("img")({
   height: "50px",
 });
@@ -30,14 +36,16 @@ const NavMenu = styled(Box)({
   display: "flex",
   alignItems: "center",
   zIndex: 1000,
-  "@media screen and (max-width: 900px)": {
-    display: "block",
-    height: "100%",
-    backgroundColor: "pink",
-    position: "fixed",
-    width: "100%",
-  },
 });
+const SideMenu = styled(Box)({
+  width: '100%',
+  height: '100vh',
+  position: 'fixed',
+  top: 0,
+  left: '100%',
+  zIndex: 1500,
+  transition: '0.5s'
+})
 const NavLink = styled(Box)({
   padding: "0 24px",
   cursor: "pointer",
@@ -76,19 +84,19 @@ function Navbar() {
   return (
     <>
       {isMobile ? (
-        <>
-          {isMenuOpen ? (
-            <NavList />
-          ) : (
+        <MobileNav>
+          <nav>
             <NavContainer>
-              <Logo src={code} alt="code navbar gif"></Logo>
-
+              <Logo src={code} alt="code navbar gif" />
               <IconButton onClick={() => setMenuOpen(true)}>
                 <Menu fontSize="large" />
               </IconButton>
             </NavContainer>
-          )}
-        </>
+          </nav>
+          <SideMenu className={`${isMenuOpen ? "active" : ""}`}>
+            <NavList />
+          </SideMenu>
+        </MobileNav>
       ) : (
         <NavContainer>
           <Logo src={code} alt="code navbar gif"></Logo>
@@ -97,11 +105,11 @@ function Navbar() {
       )}
     </>
   );
+
 }
 
 export default Navbar;
 
 //Things to improve
 //navlist view needs to be done for mobile view
-//add animation in mobile view
 //add shadow at bottom of nav container
