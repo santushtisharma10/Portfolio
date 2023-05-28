b7d9c419e6f2a5e9099f3e5360b8f68c8dc2f8f5
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-28 17:08:47 +0530

    [modification]: mobile view modified, connect button styles and dialgox box modified

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -14,26 +14,50 @@ import {
 import self from "../images/santushti.jpg";
 import bg from "../images/hero_bg.gif";
 import world from "../images/heroGif.gif";
+import { Close, GitHub, LinkedIn, Mail, Twitter } from "@mui/icons-material";
 
 const HeroSection = styled(Box)({
-  padding: "100px 50px",
+  padding: "120px 50px",
   border: "0.5px solid #F6F1F1",
   display: "grid",
   gridTemplateColumns: "65% auto",
+  "@media screen and (max-width: 900px)": {
+    gridTemplateColumns: "repeat(1, 1fr)",
+  },
 });
 const HeroContent = styled(Box)({
-  display: 'flex',
-  alignItems: 'center'
+  display: "flex",
+  alignItems: "center",
+  "@media screen and (max-width: 900px)": {
+    textAlign: "center",
+    justifyContent: "center",
+  },
 });
 const Title = styled(Typography)({
   fontSize: "64px",
   fontWeight: 800,
+  "@media screen and (max-width: 1200px)": {
+    fontSize: "48px",
+  },
 });
 const HeroImg = styled(Box)({
   display: "flex",
   justifyContent: "center",
 });
-const ConnectButton = styled(Button)({});
+const ConnectButton = styled(Button)({
+  margin: "20px 0",
+  padding: "10px 30px",
+  borderRadius: "20px",
+  borderColor: "#80cbe0",
+  color: "#80cbe0",
+  fontSize: "20px",
+  fontWeight: 600,
+  ":hover": {
+    backgroundColor: "#285cd1",
+    borderColor: "#285cd1",
+    color: "#fff",
+  },
+});
 
 function Hero() {
   const [open, setOpen] = useState(false);
@@ -45,38 +69,59 @@ function Hero() {
     <HeroSection>
       <HeroContent>
         <div>
-        <Title>
-          Hello World
-          <span>
-            <img src={world} style={{height: '50px'}} alt="content" />
-          </span>
-        </Title>
-        <Title>
-          I'm <span style={{ color: "#80cbe0" }}> Santushti Sharma </span>
-        </Title>
-        <ConnectButton variant="outlined" onClick={handleConnect}>
-          Connect with me
-        </ConnectButton>
+          <Title>
+            Hello World
+            <span>
+              <img src={world} style={{ height: "50px" }} alt="content" />
+            </span>
+          </Title>
+          <Title>
+            I'm <span style={{ color: "#80cbe0" }}> Santushti Sharma </span>
+          </Title>
+
+          <ConnectButton variant="outlined" onClick={handleConnect}>
+            Connect with me
+          </ConnectButton>
         </div>
         <Dialog open={open} onClose={handleConnect}>
           <DialogTitle
-            style={{ display: "flex", justifyContent: "space-between" }}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
           >
-            <Typography>Connect with Me</Typography>
-            <IconButton onClick={handleConnect}>Close me</IconButton>
+            <Typography style={{ fontSize: "28px", fontWeight: 700 }}>
+              Connect
+            </Typography>
+            <IconButton onClick={handleConnect}>
+              <Close />
+            </IconButton>
           </DialogTitle>
-          <DialogContent dividers style={{ display: "flex" }}>
-            <h1>LinkedIn</h1>
-            <h1>Github</h1>
-            <h1>Twitter</h1>
-            <h1>Mail</h1>
+          <DialogContent dividers style={{ display: "flex", gap: "20px" }}>
+            <IconButton color="light blue">
+              <LinkedIn fontSize="large" />
+            </IconButton>
+            <IconButton color="light blue">
+              <GitHub fontSize="large" />
+            </IconButton>
+            <IconButton color="light blue">
+              <Twitter fontSize="large" />
+            </IconButton>
+            <IconButton color="light blue">
+              <Mail fontSize="large" />
+            </IconButton>
           </DialogContent>
         </Dialog>
       </HeroContent>
 
       <HeroImg>
         <img src={self} style={{ width: "100%" }} alt="santushti sharma" />
-        <img src={bg} style={{position: 'absolute', height: '15%', right: '5%'}} alt="effect for hero section"/>
+        <img
+          src={bg}
+          style={{ position: "absolute", height: "15%", right: "5%" }}
+          alt="effect for hero section"
+        />
       </HeroImg>
     </HeroSection>
   );
@@ -85,6 +130,6 @@ function Hero() {
 export default Hero;
 
 //things to do
-//button styles
-//connect dialog box
-//mobile view of hero section
+//color scheme needs to be modified
+//use the cropped version of the photo in mobile view
+//change the position of connect button in mobile view
